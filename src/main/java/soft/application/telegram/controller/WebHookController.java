1d28d957e74d085002bbf09bc3be4fcf9d321755
adilbek <adilbek.akhmet@gmail.com>
2022-04-23 15:18:22 +0600

    make it better

---
+++ src/main/java/soft/application/telegram/controller/WebHookController.java
@@ -0,0 +1,21 @@
+package soft.application.telegram.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import soft.application.telegram.service.TelegramBotFacadeService;
+
+@RestController
+@RequiredArgsConstructor
+public class WebHookController {
+
+    private final TelegramBotFacadeService telegramBotFacadeService;
+
+    @PostMapping
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBotFacadeService.onWebhookUpdateReceived(update);
+    }
+}
