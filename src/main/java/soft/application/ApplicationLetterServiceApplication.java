e288082685b2d374d200fd6b09ffe8cd53076dc7
adilbek <adilbek.akhmet@gmail.com>
2022-04-28 22:03:41 +0600

    make it better

--- src/main/java/soft/application/ApplicationLetterServiceApplication.java
+++ src/main/java/soft/application/ApplicationLetterServiceApplication.java
@@ -1,11 +1,20 @@
 package soft.application;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+import soft.application.telegram.config.BotConfig;
 
+@Log4j2
 @SpringBootApplication
+@EnableScheduling
 @EnableAspectJAutoProxy
 @ConfigurationPropertiesScan
 public class ApplicationLetterServiceApplication {
@@ -14,4 +23,18 @@ public class ApplicationLetterServiceApplication {
         SpringApplication.run(ApplicationLetterServiceApplication.class, args);
     }
 
+    private final BotConfig botConfig;
+
+    @Autowired
+    public ApplicationLetterServiceApplication(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Scheduled(fixedDelay = 300000)
+    public void pingMe() {
+        log.info("Ping to {}", botConfig.getWebHookPath());
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(botConfig.getWebHookPath(), Object.class);
+        log.info("Ping was successful {}", botConfig.getWebHookPath());
+    }
 }
